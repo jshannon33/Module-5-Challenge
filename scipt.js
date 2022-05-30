dec691a0885b3dd62a317e933faff998870fd575
Jason Shannon <jshannon3343@gmail.com>
2022-05-29 22:00:47 -0500

    added all hour blocks

--- scipt.js
+++ scipt.js
@@ -1,9 +1,13 @@
 
-// header date
+// header date **NEEDS TO BE CONVERTED TO moment.js**
 let currentDate = document.getElementById('currentDay');
 let today = Date();
 currentDate.append(today);
-// header date
+// header date 
 
+//change to color of text areas based on time
+
+
+//function for saving textarea
 
 
