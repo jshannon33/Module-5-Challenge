826f817778b2c1fad3c942192d1ef58202205bba
Jason Shannon <jshannon3343@gmail.com>
2022-05-31 17:10:09 -0500

    save button
    the save button now works to save event to local storage

--- scipt.js
+++ scipt.js
@@ -11,9 +11,17 @@ currentDate.append(today);
 
 
 //change to color of text areas based on time
+const time = [9,10,11,12,13,14,15,16,17];
 
+let d = new Date();
+let date = d.getHours();
 
+console.log(date);
 
+console.log(time.value);
+// if (date == 16){
+//     agenda.style.backgroundColor = 'red';
+// }
 
 
 
@@ -21,12 +29,11 @@ currentDate.append(today);
 
 saveBtn = document.getElementById('saveBtn');
 
-saveBtn.click = function(){
-
-console.log('you pressed the button');
-
-};
-
 saveBtn.addEventListener('click', function(){
-    console.log('pressed it');
-})
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
