d85677fac92956e7b06222a76711da76d9fe10f3
Jason Shannon <jshannon3343@gmail.com>
2022-05-30 23:06:50 -0500

    update

--- scipt.js
+++ scipt.js
@@ -1,13 +1,32 @@
 
+let agenda = document.getElementById('agenda-text');
+
 // header date **NEEDS TO BE CONVERTED TO moment.js**
 let currentDate = document.getElementById('currentDay');
 let today = Date();
 currentDate.append(today);
 // header date 
 
+
+
+
 //change to color of text areas based on time
+
+
+
+
 
 
 //function for saving textarea
 
+saveBtn = document.getElementById('saveBtn');
 
+saveBtn.click = function(){
+
+console.log('you pressed the button');
+
+};
+
+saveBtn.addEventListener('click', function(){
+    console.log('pressed it');
+})
