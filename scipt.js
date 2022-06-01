a3752d6741a238a8ba140918ad5c11ecd2c42fb5
Jason Shannon <jshannon3343@gmail.com>
2022-05-31 19:02:43 -0500

    all buttons work
    all buttons now save to local storage. will find a way to not have to duplicate code.

--- scipt.js
+++ scipt.js
@@ -18,18 +18,110 @@ let date = d.getHours();
 
 console.log(date);
 
-console.log(time.value);
-// if (date == 16){
+
+// if (date == time){
 //     agenda.style.backgroundColor = 'red';
 // }
 
+
+const greaterThanTen = time.find(element => element < date);
+
+console.log(greaterThanTen);
 
 
 //function for saving textarea
 
 saveBtn = document.getElementById('saveBtn');
+saveBtn2 = document.getElementById('saveBtn2');
+saveBtn3 = document.getElementById('saveBtn3');
+saveBtn4 = document.getElementById('saveBtn4');
+saveBtn5 = document.getElementById('saveBtn5');
+saveBtn6 = document.getElementById('saveBtn6');
+saveBtn7 = document.getElementById('saveBtn7');
+saveBtn8 = document.getElementById('saveBtn8');
+saveBtn9 = document.getElementById('saveBtn9');
 
 saveBtn.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn2.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn3.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn4.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn5.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn6.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn7.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn8.addEventListener('click', function(){
+    const key = agenda.value;
+
+    localStorage.setItem(key, '');
+
+    console.log(key);
+
+});
+
+
+saveBtn9.addEventListener('click', function(){
     const key = agenda.value;
 
     localStorage.setItem(key, '');
